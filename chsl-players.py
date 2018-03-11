4dedb6bbf9314852c45069cef1a0da562de40037
Igor Korostil <eeghor@gmail.com>
2018-03-12 00:36:13 +1100

    all squads

--- chsl-players.py
+++ chsl-players.py
@@ -1,88 +1,126 @@
 import requests
 from bs4 import BeautifulSoup
 
-wiki_url = 'https://en.wikipedia.org'
-main_url = 'https://en.wikipedia.org/wiki/Chinese_Super_League'
 
-players = set()
+class ChineseSLScraper:
 
-r = requests.get(main_url) 
+	def __init__(self):
 
-soup = BeautifulSoup(r.text, 'lxml')
+		self.wiki_url = 'https://en.wikipedia.org'
+		self.main_url = 'https://en.wikipedia.org/wiki/Chinese_Super_League'
+		self.team_urls = {}
 
-# find current team names and urls
-team_urls = {}
+		self.players = set()
 
-_h2 = None
+	def _get_club_urls(self):
 
-for _ in soup.find_all(id='Current_clubs'):
-	if len({'current', 'clubs'} & set(_.text.strip().lower().split())) == 2:
-		_h2 = _.parent
-		break
+		soup = BeautifulSoup(requests.get(self.main_url).text, 'lxml')	
 
-if _h2:
+		_h2 = None
 
-	try:
-		tb = _h2.find_next_sibling('table', 'wikitable')
-		print('found team name table')
-	except:
-		print('can\'t find the table with team names!')
+		for _ in soup.find_all(id='Current_clubs'):
+			if not ({'current', 'clubs'} - set(_.text.strip().lower().split())):
+				_h2 = _.parent
+				break
 
-	for row in tb.find_all('tr'):
+		if _h2:
 
-		td = row.find('td')
+			try:
+				tb = _h2.find_next_sibling('table', 'wikitable')
+			except:
+				print('can\'t find the table with team names!')
+				return self
 
-		if td:
-			a = td.find('a')
-			team_urls.update({a.text.lower().strip(): wiki_url + a['href']})
-		else:
-			continue
-
-print(f'total {len(team_urls)} teams')
-print(team_urls)
-
-for team in team_urls:
-
-	print(f'scraping {team}...')
-
-	soup = BeautifulSoup(requests.get(team_urls[team]).text, 'lxml')
-
-	_h2 = None
-
-	for _ in soup.find_all(id='Current_squad'):
-
-		_h2 = _.parent
-		break
-
-	if _h2:
-
-		try:
-			tb = _h2.find_next_sibling('table')
-		except:
-			print('no table with players')
-
-		for row in tb.find_all(class_='vcard agent'):
-
-			for i, td in enumerate(row.find_all('td')):
-
-				if i == 1:
-					try:
-						sp = td.find('span')
-					except:
+			rows = tb.find_all('tr')
+			
+			if not rows:
+				print('there are no rows in the current clubs table!')
+				return self
+			else:
+				for row in rows:
+			
+					td = row.find('td')
+			
+					if td:
+						a = td.find('a')
+						if a:
+							self.team_urls.update({a.text.lower().strip(): self.wiki_url + a['href']})
+					else:
 						continue
-					try:
-						a = sp.find('a')
-					except:
-						continue
+			return self
 
-				if (i == 3) and (a['title'].lower() == 'china'):
-					players.add(td.text.lower().strip().split('(')[0].strip())
 
-	print(f'players so far: {len(players)}')
+	def _get_squad_table(self, url, span_id):
 
-with open('chinese_players.txt','w') as f:
-	for p in players:
-		f.write(f'{p}\n')
+
+		soup = BeautifulSoup(requests.get(url).text, 'lxml')
+
+		_h2 = None
+
+		right_ids = soup.find_all(id=span_id)
+
+		if right_ids:
+
+			for _ in soup.find_all(id=span_id):
+				_h2 = _.parent
+				break
+	
+			if _h2:
+	
+				tb = _h2.find_next_sibling('table')
+				
+				if not tb:
+					print('found a header but no table!')
+					return self
+
+				else:
+				
+					rows_pl = tb.find_all(class_='vcard agent')
+	
+					if rows_pl:
+	
+						for row in rows_pl:
+			
+							for i, td in enumerate(row.find_all('td')):
+			
+								if i == 1:
+									try:
+										sp = td.find('span')
+									except:
+										continue
+									try:
+										a = sp.find('a')
+									except:
+										continue
+			
+								if (i == 3) and (a['title'].lower() == 'china'):
+									self.players.add(td.text.lower().strip().split('(')[0].strip())
+	
+		return self
+
+	def get_names(self):
+
+		self._get_club_urls()
+
+		for team in self.team_urls:
+
+			print(f'{team.upper()}...')
+
+			for span_id in 'Current_squad First_team Reserve_team On_loan Reserve_squad First_team_squad'.split():
+				self._get_squad_table(self.team_urls[team], span_id)
+
+		return self
+
+	def save_to_file(self, file_name):
+
+		with open(file_name,'w') as f:
+			for p in self.players:
+				f.write(f'{p}\n')
+		print(f'saved {len(self.players)} player names to {file_name}')
+
+if __name__ == '__main__':
+
+	c = ChineseSLScraper().get_names().save_to_file('chinese_superleague_players.txt')
 
 
 
